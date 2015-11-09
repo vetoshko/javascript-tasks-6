a8736433609b2a7869890fdf62c671fdc7575e57
vetoshko <vetoshkodasha@gmail.com>
2015-11-10 02:46:13 +0500

    Commit

--- index.js
+++ index.js
@@ -25,37 +25,37 @@ var robberyMoment = robbery.getAppropriateMoment(
     }
 );
 
-// Вышлем приглашение:
-// - %DD - день недели (ПН, ВТ, СР, ЧТ, ПТ, СБ, ВС)
-// - %HH - часы
-// - %MM - минуты
-console.log(
-    robberyMoment.format('Ограбление должно состоятся в %DD. Всем быть готовыми к %HH:%MM!')
-);
-// Ограбление должно состоятся в ВТ. Всем быть готовыми к 03:30!
-
-// Для Дарьи вышлем приглашение в её часовом поясе, так как она живёт на Тортуге (часовой пояс -5):
-robberyMoment.timezone = -5; // Переводим время в часовой пояс Дарьи
-console.log(
-    robberyMoment.format('Дарья, прилетай в Котеринбург в %DD. Собираемся в %HH:%MM!')
-);
-robberyMoment.timezone = 5; // Переводим время в часовой пояс Котеринбурга
-
-
-// Необязательное задание (+40 к смекалке)
-
-var currentMoment = moment();
-
-// Как во всех фильмах про ограбления мы будем писать:
-
-currentMoment.date = 'ПН 09:01+5';
-console.log(
-    robbery.getStatus(currentMoment, robberyMoment)
-);
-// «До ограбления остался 1 день 6 часов 59 минут»
-
-currentMoment.date = 'ПН 12:59+5';
-console.log(
-    robbery.getStatus(currentMoment, robberyMoment)
-);
-// «До ограбления остался 21 час 1 минута»
+// // Вышлем приглашение:
+// // - %DD - день недели (ПН, ВТ, СР, ЧТ, ПТ, СБ, ВС)
+// // - %HH - часы
+// // - %MM - минуты
+// console.log(
+//     robberyMoment.format('Ограбление должно состоятся в %DD. Всем быть готовыми к %HH:%MM!')
+// );
+// // Ограбление должно состоятся в ВТ. Всем быть готовыми к 03:30!
+//
+// // Для Дарьи вышлем приглашение в её часовом поясе, так как она живёт на Тортуге (часовой пояс -5):
+// robberyMoment.timezone = -5; // Переводим время в часовой пояс Дарьи
+// console.log(
+//     robberyMoment.format('Дарья, прилетай в Котеринбург в %DD. Собираемся в %HH:%MM!')
+// );
+// robberyMoment.timezone = 5; // Переводим время в часовой пояс Котеринбурга
+//
+//
+// // Необязательное задание (+40 к смекалке)
+//
+// var currentMoment = moment();
+//
+// // Как во всех фильмах про ограбления мы будем писать:
+//
+// currentMoment.date = 'ПН 09:01+5';
+// console.log(
+//     robbery.getStatus(currentMoment, robberyMoment)
+// );
+// // «До ограбления остался 1 день 6 часов 59 минут»
+//
+// currentMoment.date = 'ПН 12:59+5';
+// console.log(
+//     robbery.getStatus(currentMoment, robberyMoment)
+// );
+// // «До ограбления остался 21 час 1 минута»
--- robbery.js
+++ robbery.js
@@ -5,14 +5,71 @@ var moment = require('./moment');
 // Выбирает подходящий ближайший момент начала ограбления
 module.exports.getAppropriateMoment = function (json, minDuration, workingHours) {
     var appropriateMoment = moment();
+    var data = JSON.parse(json);
+    var newRobbery = getNewRobbery(data);
+    var openHour = parseTime(workingHours["from"]);
+    var closeHour = parseTime(workingHours["to"]);
+    var hoursDuration = Math.floor(minDuration / 60);
+    var minutesDuration = minDuration % 60;
+    var msInDay = 24*3600*1000;
+    var tempDate = new Date().getTime()
+    for (var days = 0; days < 8; days++) {
+        var currentDate = new Date(tempDate + days * msInDay);
+        if (currentDate.getDay() > 0 && currentDate.getDay() < 4) {
+            console.log(newRobbery[currentDate.getDay()]);
 
-    // 1. Читаем json
-    // 2. Находим подходящий ближайший момент начала ограбления
-    // 3. И записываем в appropriateMoment
-
+        }
+    }
     return appropriateMoment;
 };
 
+
+function getNewRobbery(data) {
+    var newRobbery = {}
+    for (var name in data) {
+        for (var day = 0; day < data[name].length; day++) {
+            var fixed_day = day + 1;
+            if (typeof newRobbery[fixed_day] == 'undefined')
+                newRobbery[fixed_day] = [];
+            if (typeof newRobbery[fixed_day][name] == 'undefined')
+                newRobbery[fixed_day][name] = [];
+            var time = {};
+            for (var prop in data[name][day]) {
+                var correctTime = getCorrectTime(data[name][day][prop]);
+                time[prop] = correctTime;
+            }
+            newRobbery[fixed_day][name].push(time);
+        }
+    }
+    return newRobbery;
+}
+
+
+function getCorrectTime(time) {
+    var newDate = parseTime(time);
+    return newDate;
+}
+
+function parseTime(timeStr) {
+
+    var wordToNumber = {
+        'ПН' : 1,
+        'ВТ' : 2,
+        'СР' : 3
+    }
+    var newDate = {}
+    if (timeStr.length == 10) {
+        newDate.date = wordToNumber[timeStr.substr(0, 2)];
+        newDate.hours = timeStr.substr(3, 2);
+        newDate.minutes = timeStr.substr(6, 2);
+        newDate.timezone = timeStr.substr(8, 2);
+    } else {
+        newDate.hours = timeStr.substr(0, 2);
+        newDate.minutes = timeStr.substr(3, 2);
+        newDate.timezone = timeStr.substr(6, 2);
+    }
+    return newDate;
+}
 // Возвращает статус ограбления (этот метод уже готов!)
 module.exports.getStatus = function (moment, robberyMoment) {
     if (moment.date < robberyMoment.date) {
